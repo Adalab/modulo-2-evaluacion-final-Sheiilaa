993c5a92418af5c77afd315b4d30bd8ad1ada22c
sheila <sheila18.sa@gmail.com>
2021-09-09 15:15:49 +0200

    first passes in the final project

---
+++ public/assets/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Primero hacemos las variables
+
+const searchFilm= document.querySelector('.js_search-films');
+const button= document.querySelector('.js_button');
+const reset= document.querySelector('.js_reset');
+const films= document.querySelector('.js_films');
+const favoriteFilms= document.querySelector('.js_favorite-films');
+
+}
+
+
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Primero hacemos las variables
+
+const searchFilm= document.querySelector('.js_search-films');
+const button= document.querySelector('.js_button');
+const reset= document.querySelector('.js_reset');
+const films= document.querySelector('.js_films');
+const favoriteFilms= document.querySelector('.js_favorite-films');
+
+}
+
